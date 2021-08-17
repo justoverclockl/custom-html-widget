759ab7c54547292629c7a75fdd6770977e46a1a7
Justoverclock <79002016+justoverclockl@users.noreply.github.com>
2021-08-17 17:50:50 +0200

    live preview

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -18,12 +18,21 @@ app.initializers.add('justoverclock/custom-html-widget', () => {
       .registerSetting(function () {
         const warningMessage = app.translator.trans('justoverclock-custom-html-widget.admin.warningmessage');
         const warnintTitle = app.translator.trans('justoverclock-custom-html-widget.admin.warningtext');
+        // anteprima in tempo reale
+        $("textarea").keyup(function() {
+          const srcdoccontent = $("#htmltext").val();
+          $("#liveoutp").attr("srcdoc", srcdoccontent);
+        });
+        //fine anteprima in tempo reale
         return (
             <div className="Form-group settingscustomhtml">
                 <label>{warnintTitle}</label>
                 <aside class="customhtmlwarning">{warningMessage}</aside>
-                <textarea className="FormControl customhtmlbox" bidi={this.setting('justoverclock-custom-html-widget.customCode')}></textarea>
-            </div>
+              <div class="textandprw">
+                <textarea id="htmltext" className="FormControl customhtmlbox" bidi={this.setting('justoverclock-custom-html-widget.customCode')}/>
+                <iframe class="liveoutp" id="liveoutp"></iframe>
+              </div>
+              </div>
         );
     });
 });
